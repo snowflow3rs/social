a078e833160e8af2a147f2cf01c7a22c8f204579
unknown <zero2392001@gmail.com>
2023-10-31 17:51:36 +0700

    fixing like posting

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -34,6 +34,7 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
           community={thread.community}
           createdAt={thread.createdAt}
           comments={thread.children}
+          likes={thread.likes}
         />
       ))}
     </section>
